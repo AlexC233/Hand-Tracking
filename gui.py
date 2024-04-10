bf1d93159c63e3b0afb0a0a6d044d7965165df29
Robert Podrebarac <robbie.podrebarac@gmail.com>
2024-04-10 15:02:29 -0400

    Images Uploaded to the Grid

--- gui.py
+++ gui.py
@@ -1,14 +1,32 @@
 import tkinter as tk
+from PIL import ImageTk, Image
 
 # SETUP.
 root = tk.Tk()
+#canvas = tk.Canvas(root, width=root.winfo_width(), height=root.winfo_height())
 
 
 # Errors.
-flat_fingers = "Lift Your Fingers!"
-raise_wrists = "Raise Your Wrists!"
-flying_pinkie = "Bend Your Pinky!"
-thumb_falling = "Put Your Thumb on the Keyboard!"
+flat_fingers = {
+    "text": "Lift Your Fingers!",
+    "image": ImageTk.PhotoImage(Image.open("./images/raise_wrists.png"))
+}
+
+raise_wrists = {
+    "text": "Raise Your Wrists!",
+    "image": ImageTk.PhotoImage(Image.open("./images/raise_wrists.png"))
+}
+
+flying_pinkie = {
+    "text": "Bend Your Pinky!",
+    "image": ImageTk.PhotoImage(Image.open("./images/raise_wrists.png"))
+}
+
+thumb_falling = {
+    "text": "Put Your Thumb on the Keyboard!",
+    "image": ImageTk.PhotoImage(Image.open("./images/raise_wrists.png"))
+}
+
 posture_corrections = [[flat_fingers, raise_wrists], [flying_pinkie, thumb_falling]]
 
 
@@ -23,9 +41,10 @@ for i in range(2):
             relief=tk.RAISED,
             borderwidth=1
         )
+
         frame.grid(row=i, column=j, padx=5, pady=5)
-        error_label = tk.Label(text=posture_corrections[i][j], master=frame)
-        posture_corrections[i][j]
+        error_label = tk.Label(image=posture_corrections[i][j]["image"], master=frame)
+        image_label = tk.Label(image=posture_corrections[i][j]["image"], master=frame)
         error_label.pack(padx=5, pady=5)
 
 
