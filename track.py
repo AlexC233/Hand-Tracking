37d3170cae0ecea3103f9917fbb6d38d9da0ebf7
Alex Chen <1513214585@qq.com>
2024-04-11 00:31:37 -0400

    results from multiple cameras are now merged into a single result

--- track.py
+++ track.py
@@ -93,24 +93,24 @@ def get_all_angles(joints:np.array) -> np.array:
 
 # Setup code
 cam1 = cv2.VideoCapture(0)
-# cam2 = cv2.VideoCapture(2)
+cam2 = cv2.VideoCapture(2)
 
 (length, width) = (640, 480)
 
 # set the cameras to 640x480
-cam1.set(3, length)
-cam1.set(4, width)
+# cam1.set(3, length)
+# cam1.set(4, width)
 # cam2.set(3, length)
 # cam2.set(4, width)
 
-cam2 = None
+# cam2 = None
 
 inverted = False
 
 raw = {"cam1":
     {"use": True, "cam": cam1, "image": None},
     "cam2":
-    {"use": False, "cam": cam2, "image": None},
+    {"use": True, "cam": cam2, "image": None},
 }
 
 cameras = [key for key in raw if raw[key]["use"]]
@@ -130,6 +130,12 @@ results = {"cam1":
             {"detected": False, "coordinates": np.empty((0, 3)), "screen_pos": np.empty((0, 2)), "angles": np.empty(0)},
         }
     }
+
+merged_results = {"left":
+    {"detected": False, "coordinates": np.empty((0, 3)), "angles": np.empty(0)},
+    "right":
+    {"detected": False, "coordinates": np.empty((0, 3)), "angles": np.empty(0)}
+}
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(static_image_mode=False,
@@ -172,7 +178,18 @@ def get_coordinates(cam:str, hand, handedness, handnum:int)->None:
         # cv2.putText(img, str(id) + " " + str(rcoord), (cx,cy), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,255), 1)
         results[cam][handedness]["coordinates"] = np.vstack((results[cam][handedness]["coordinates"], coord))
         
-def label_image(cam:str, handedness)->None:
+    # using world coordinates instead of screen coordinates
+    # for id, lm in enumerate(hand.multi_hand_landmarks[handnum].landmark):
+    #     h, w, c = raw[cam]["image"].shape
+    #     cx, cy = int(lm.x *w), int(lm.y*h)
+    #     results[cam][handedness]["screen_pos"] = np.vstack((results[cam][handedness]["screen_pos"], np.array([cx, cy])))
+    #     cv2.circle(raw[cam]["image"], (cx,cy), 3, (255,0,255), cv2.FILLED)
+    
+    # for id, lm in enumerate(hand.multi_hand_world_landmarks[handnum].landmark):
+    #     coord = np.array([lm.x, lm.y, lm.z])
+    #     results[cam][handedness]["coordinates"] = np.vstack((results[cam][handedness]["coordinates"], coord))
+        
+def display_angles(cam:str, handedness)->None:
     cv2.putText(raw[cam]["image"], str(np.round(results[cam][handedness]["angles"][0] , 1)), (int(results[cam][handedness]["screen_pos"][2] [0]), int(results[cam][handedness]["screen_pos"][2] [1])), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,255), 1) # Thumb MCP
     cv2.putText(raw[cam]["image"], str(np.round(results[cam][handedness]["angles"][1] , 1)), (int(results[cam][handedness]["screen_pos"][3] [0]), int(results[cam][handedness]["screen_pos"][3] [1])), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,255), 1) # Thumb IP
     cv2.putText(raw[cam]["image"], str(np.round(results[cam][handedness]["angles"][2] , 1)), (int(results[cam][handedness]["screen_pos"][6] [0]), int(results[cam][handedness]["screen_pos"][6] [1])), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,255), 1) # Index MCP
@@ -184,6 +201,10 @@ def label_image(cam:str, handedness)->None:
     cv2.putText(raw[cam]["image"], str(np.round(results[cam][handedness]["angles"][8] , 1)), (int(results[cam][handedness]["screen_pos"][18][0]), int(results[cam][handedness]["screen_pos"][18][1])), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,255), 1) # Pinky MCP
     cv2.putText(raw[cam]["image"], str(np.round(results[cam][handedness]["angles"][9] , 1)), (int(results[cam][handedness]["screen_pos"][19][0]), int(results[cam][handedness]["screen_pos"][19][1])), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,255), 1) # Pinky PIP
     cv2.putText(raw[cam]["image"], str(np.round(results[cam][handedness]["angles"][10], 1)), (int(results[cam][handedness]["screen_pos"][0] [0]), int(results[cam][handedness]["screen_pos"][0] [1])), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,255), 1) # Wrist Elevation
+
+def display_coordinates(cam:str, handedness)->None:
+    for i in range(21):
+        cv2.putText(raw[cam]["image"], str(np.round(results[cam][handedness]["coordinates"][i], 2)), (int(results[cam][handedness]["screen_pos"][i][0]), int(results[cam][handedness]["screen_pos"][i][1])), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,255), 1)
 
 def display_image(cam:str)->None:
     cv2.imshow(cam, raw[cam]["image"])
@@ -209,7 +230,8 @@ def get_hand(cam:str, hand, handnum)->None:
     get_coordinates(cam, hand, handedness, handnum)
     results[cam][handedness]["angles"] = get_all_angles(results[cam][handedness]["coordinates"])
     
-    label_image(cam, handedness)
+    display_angles(cam, handedness)
+    # display_coordinates(cam, handedness)
     
 def process_hands(cam:str)->None:
     camlandmarks = results[cam]["lms"]
@@ -223,7 +245,7 @@ def process_hands(cam:str)->None:
             get_hand(cam, camlandmarks, 0)
             get_hand(cam, camlandmarks, 1)
             
-def run(cam:str)->None:
+def run_track(cam:str)->None:
     get_image(cam)
     process_image(cam)
     process_hands(cam)
@@ -231,17 +253,72 @@ def run(cam:str)->None:
     display_image(cam)
             
 def track()->None:
-    try:
-        for cam in cameras:
-            run(cam)
-    except KeyboardInterrupt:
-        for key in raw:
-            raw[key]["cam"].release()
-        cv2.destroyAllWindows()
-        return
-    except Exception as e:
-        print(e)
+    for cam in cameras:
+        run_track(cam)
+
+def distance_from_center(coord:np.array)->float:
+    # return the distance from the center of the screen, which is (0.5, 0.5)
+    return np.linalg.norm(coord[:2] - np.array([0.5, 0.5]))
+        
+def get_results()->None:
+    ''' Process the results. Merge the results from all cameras.
+        Take the average of the angles.
+        Keep the coordinates from the camera with the readings closest to the center of the screen.
+    '''
+    # reset the merged results
+    for hand in merged_results:
+        merged_results[hand]["detected"] = False
+        merged_results[hand]["coordinates"] = np.empty((0, 3))
+        merged_results[hand]["angles"] = np.empty(0)
+        
+    # merge
+    left_angles = np.empty((0, 11))
+    right_angles = np.empty((0, 11))
+
+    left_coords = np.empty((21, 3, 0))
+    right_coords = np.empty((21, 3, 0))
+    for cam in cameras:
+        for hand in ("left", "right"):
+            if results[cam][hand]["detected"]:
+                if hand == "left":
+                    left_angles = np.vstack((left_angles, results[cam][hand]["angles"]))
+                    left_coords = np.dstack((left_coords, results[cam][hand]["coordinates"]))
+                    
+                else:
+                    right_angles = np.vstack((right_angles, results[cam][hand]["angles"]))
+                    right_coords = np.dstack((right_coords, results[cam][hand]["coordinates"]))
+                    
+    # average the angles
+    if left_angles.size:
+        merged_results["left"]["angles"] = np.mean(left_angles, axis=0)
+        merged_results["left"]["detected"] = True
+    if right_angles.size:
+        merged_results["right"]["angles"] = np.mean(right_angles, axis=0)
+        merged_results["right"]["detected"] = True
+        
+    # get the coordinates closest to the center of the screen
+    if left_coords.size:
+        left_distances = np.array([distance_from_center(coord) for coord in left_coords])
+        closest_left = np.argmin(left_distances)
+        merged_results["left"]["coordinates"] = left_coords[closest_left]
+    if right_coords.size:
+        right_distances = np.array([distance_from_center(coord) for coord in right_coords])
+        closest_right = np.argmin(right_distances)
+        merged_results["right"]["coordinates"] = right_coords[closest_right]          
+        
+    # test prints
+    # check if a hand is detected
+    # if merged_results["left"]["detected"]:
+    #     print("Left Hand Detected")
+    # if merged_results["right"]["detected"]:
+    #     print("Right Hand Detected")
 
 if __name__ == "__main__":    
     while True:
-        track()
+        try:
+            track()
+            get_results()
+        except KeyboardInterrupt:
+            for key in raw:
+                raw[key]["cam"].release()
+            cv2.destroyAllWindows()
