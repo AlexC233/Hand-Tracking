c198f89004f141b939430cec2df382238b5b3329
Robert Podrebarac <robbie.podrebarac@gmail.com>
2024-04-14 16:05:24 -0400

    Implemented Posture Error Images in the Grid

--- gui.py
+++ gui.py
@@ -3,28 +3,29 @@ from PIL import ImageTk, Image
 
 # SETUP.
 root = tk.Tk()
-#canvas = tk.Canvas(root, width=root.winfo_width(), height=root.winfo_height())
-
+WIDTH = 1980
+HEIGHT = 1080
+# root.maxsize(1920, 1820)
 
 # Errors.
 flat_fingers = {
     "text": "Lift Your Fingers!",
-    "image": ImageTk.PhotoImage(Image.open("./images/raise_wrists.png"))
+    "image": ImageTk.PhotoImage(Image.open("./images/Wrist_TopLeft.png").resize(((WIDTH//4, HEIGHT//4))))
 }
 
 raise_wrists = {
     "text": "Raise Your Wrists!",
-    "image": ImageTk.PhotoImage(Image.open("./images/raise_wrists.png"))
+    "image": ImageTk.PhotoImage(Image.open("./images/Fingers_TopRight.png").resize((WIDTH//4, HEIGHT//4)))
 }
 
 flying_pinkie = {
     "text": "Bend Your Pinky!",
-    "image": ImageTk.PhotoImage(Image.open("./images/raise_wrists.png"))
+    "image": ImageTk.PhotoImage(Image.open("./images/Thumb_BottomLeft.png").resize((WIDTH//4, HEIGHT//4)))
 }
 
 thumb_falling = {
     "text": "Put Your Thumb on the Keyboard!",
-    "image": ImageTk.PhotoImage(Image.open("./images/raise_wrists.png"))
+    "image": ImageTk.PhotoImage(Image.open("./images/Pinky_BottomRight.png").resize((WIDTH//4, HEIGHT//4)))
 }
 
 posture_corrections = [[flat_fingers, raise_wrists], [flying_pinkie, thumb_falling]]
@@ -32,9 +33,9 @@ posture_corrections = [[flat_fingers, raise_wrists], [flying_pinkie, thumb_falli
 
 # GRID.
 for i in range(2):
-    root.columnconfigure(i, weight=1, minsize=root.winfo_screenwidth()/2)
-    root.rowconfigure(i, weight=1, minsize=root.winfo_screenheight()/2)
-    
+    root.columnconfigure(i, weight=1, minsize=WIDTH//4)
+    root.rowconfigure(i, weight=1, minsize=HEIGHT//4)
+
     for j in range(2):
         frame = tk.Frame(
             master=root,
@@ -42,10 +43,10 @@ for i in range(2):
             borderwidth=1
         )
 
-        frame.grid(row=i, column=j, padx=5, pady=5)
+        frame.grid(row=i, column=j)
         error_label = tk.Label(image=posture_corrections[i][j]["image"], master=frame)
         image_label = tk.Label(image=posture_corrections[i][j]["image"], master=frame)
-        error_label.pack(padx=5, pady=5)
+        error_label.pack()
 
 
 
