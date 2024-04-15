9cdad98d971d6f83d5ca063e98c6463250113080
Robert Podrebarac <robbie.podrebarac@gmail.com>
2024-04-14 23:14:00 -0400

    Implemented (Without Dinosaur Image)

--- gui.py
+++ gui.py
@@ -3,57 +3,63 @@ from PIL import ImageTk, Image
 
 # SETUP.
 root = tk.Tk()
-WIDTH = 1980
-HEIGHT = 1080
-# root.maxsize(1920, 1820)
+WIDTH = 1600
+HEIGHT = 900
+root.geometry("1600x900")
 
 # Errors.
 flat_fingers = {
     "text": "Lift Your Fingers!",
-    "image": ImageTk.PhotoImage(Image.open("./images/Wrist_TopLeft.png").resize(((WIDTH//4, HEIGHT//4))))
+    "image": ImageTk.PhotoImage(Image.open("./images/Wrist_TopLeft.png").resize(((WIDTH//2, HEIGHT//2))))
 }
 
 raise_wrists = {
     "text": "Raise Your Wrists!",
-    "image": ImageTk.PhotoImage(Image.open("./images/Fingers_TopRight.png").resize((WIDTH//4, HEIGHT//4)))
+    "image": ImageTk.PhotoImage(Image.open("./images/Fingers_TopRight.png").resize((WIDTH//2, HEIGHT//2)))
 }
 
 flying_pinkie = {
     "text": "Bend Your Pinky!",
-    "image": ImageTk.PhotoImage(Image.open("./images/Thumb_BottomLeft.png").resize((WIDTH//4, HEIGHT//4)))
+    "image": ImageTk.PhotoImage(Image.open("./images/Thumb_BottomLeft.png").resize((WIDTH//2, HEIGHT//2)))
 }
 
 thumb_falling = {
     "text": "Put Your Thumb on the Keyboard!",
-    "image": ImageTk.PhotoImage(Image.open("./images/Pinky_BottomRight.png").resize((WIDTH//4, HEIGHT//4)))
+    "image": ImageTk.PhotoImage(Image.open("./images/Pinky_BottomRight.png").resize((WIDTH//2, HEIGHT//2)))
 }
 
 posture_corrections = [[flat_fingers, raise_wrists], [flying_pinkie, thumb_falling]]
 
 
-# GRID.
-for i in range(2):
-    root.columnconfigure(i, weight=1, minsize=WIDTH//4)
-    root.rowconfigure(i, weight=1, minsize=HEIGHT//4)
+# PLACE THE FRAMES.
+frame = tk.Frame(root, width=WIDTH, height=HEIGHT)
+frame.place(anchor="nw", x=0, y=0)
 
-    for j in range(2):
-        frame = tk.Frame(
-            master=root,
-            relief=tk.RAISED,
-            borderwidth=1
-        )
+# IMAGES.
+def display_error(errors:tuple):
+    '''Display the errors of the user.'''
 
-        frame.grid(row=i, column=j)
-        error_label = tk.Label(image=posture_corrections[i][j]["image"], master=frame)
-        image_label = tk.Label(image=posture_corrections[i][j]["image"], master=frame)
-        error_label.pack()
+    if errors[0]:
+        flat_fingers_label = tk.Label(image=posture_corrections[0][0]["image"], master=frame)
+        flat_fingers_label.place(anchor="nw", x=0, y=0)
 
+    if errors[1]:
+        raise_wrists_label = tk.Label(image=posture_corrections[0][1]["image"], master=frame)
+        raise_wrists_label.place(anchor="nw", x=WIDTH//2, y=0)
+
+    if errors[2]:
+        flying_pinkie_label = tk.Label(image=posture_corrections[1][0]["image"], master=frame)
+        flying_pinkie_label.place(anchor="nw", x=0, y=HEIGHT//2)
+
+    if errors[3]:
+        thumb_falling_label = tk.Label(image=posture_corrections[1][1]["image"], master=frame)
+        thumb_falling_label.place(anchor="nw", x=WIDTH//2, y=HEIGHT//2)
+
+    # USE A PRINT-OUT. NOT SUPPORTED.
+    # dinosaur_label = tk.Label(image=ImageTk.PhotoImage(Image.open("./images/Dino_Centre.png").resize((WIDTH//8, HEIGHT//8))), master=frame)
+    # dinosaur_label.place(anchor="center", relx=0.5, rely=0.5)
+
+display_error((True, True, True, True))
 
 
 root.mainloop()
-
-
-## OTHER CODE.
-# for i in range(len(posture_corrections)):
-#     posture_corrections[i].pack()
-#     frames[i].pack()
